a7a0652701a0b25274083a765a5f607531461302
Zirou Kamikaze <kamikaze.zirou@gmail.com>
2021-02-22 00:41:31 +0900

    refactor: repository層を追加

--- lib/src/service/task_service.dart
+++ lib/src/service/task_service.dart
@@ -1,23 +1,15 @@
 import 'package:todo/src/domain/task.dart';
+import 'package:todo/src/repository/task_repository.dart';
 
 class TaskService {
-  var tasks = <Task>[
-    Task(id: 1, title: 'task1', description: 'desc1'),
-    Task(id: 2, title: 'task2', description: 'desc2'),
-  ];
+  final TaskRepository repository = TaskRepository();
 
   Future<Task> createTask(Task task) async {
-    var newTask = Task(
-      id: tasks.length + 1,
-      title: task.title,
-      description: task.description,
-    );
-    tasks.add(newTask);
-    return newTask;
+    return repository.createTask(task);
   }
 
   @override
   Future<List<Task>> ListTask() async {
-    return tasks;
+    return repository.ListTask();
   }
 }
